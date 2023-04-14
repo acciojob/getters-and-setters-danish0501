0208cace48cb17ce28cd365cf948adf1d284d14d
danish0501 <alidanish373@gmail.com>
2023-04-14 23:41:43 +0530

    getters and setters

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //complete this code
 class Person {
     constructor(name, age) {
-        this.name = name;
-        this.age = age;
+        this._name = name;
+        this._age = age;
     }
     get getname() {
-        return this.name;
+        return this._name;
     }
 
     set setage(age) {
-        this.age = age;
+        this._age = age;
     }
 }
 
